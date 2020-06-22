814187f2df37db34f95e6c9ee508ac909bd778cb
Latinovits Antal <latinovitsantal@gmail.com>
2020-06-22 23:26:33 +0200

    Add neo4j java driver and kotlin coroutines dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,11 @@ repositories {
 }
 
 dependencies {
+    fun coroutines(module: String) = "org.jetbrains.kotlinx:kotlinx-coroutines-$module:1.3.7"
+
     implementation(kotlin("stdlib-jdk8"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.3.7")
+    implementation("org.neo4j.driver:neo4j-java-driver:4.0.2")
 }
 
 tasks {
